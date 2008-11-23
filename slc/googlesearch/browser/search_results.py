a3bcbb05b59c706ef9079210c316d6554f19ffcf
thomas <thomas@51ee9ba4-e84d-4896-84de-354f1821c169>
2008-11-23 14:23:03 +0000

    added search field above results frame

--- slc/googlesearch/browser/search_results.py
+++ slc/googlesearch/browser/search_results.py
@@ -1,13 +1,19 @@
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
+from slc.googlesearch.interfaces import IGoogleSearchSettings
 
 class SearchResultsView(BrowserView):
     """View for displaying Google CSE Search results
     """
     template = ViewPageTemplateFile('templates/search_results.pt')
-    template.id = "faq_view"
     
     def __call__(self):
         self.request.set('disable_border', True)
+        purl = getToolByName(self.context, 'portal_url')
+        portal = purl.getPortalObject()
+        self.settings = IGoogleSearchSettings(portal)
         return self.template()
+
+    def getCx(self):
+        return getattr(self.settings, 'cx', '')
