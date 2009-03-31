1f1aa2b30f0a3bad1f0528461666ff6cfb9ffb5d
thomas <thomas@51ee9ba4-e84d-4896-84de-354f1821c169>
2009-03-31 10:53:51 +0000

    only redirect if the value passed as CSE differs from the cerrently selected cx or cref

--- slc/googlesearch/browser/search_results.py
+++ slc/googlesearch/browser/search_results.py
@@ -13,7 +13,7 @@ class SearchResultsView(BrowserView):
     template.id = "slc_cse_search_results"
     
     
-    def __call__(self):      
+    def __call__(self):
         qs = unquote(self.request.get('QUERY_STRING'))
         parts = qs.split('&')
         params = dict()
@@ -23,57 +23,55 @@ class SearchResultsView(BrowserView):
                 params[key] = val
             except:
                 # bungled or missing QS
-                pass 
-
-        print "params:", params
-        # if a CSE value is present, invalidate cx and cref, because we are switching source
+                pass
+        
+        # if a CSE value is present, invalidate cx and cref, because we are switching source;
+        # but only if the cse value differs from the current selection
         if params.has_key('cse'):
             cse = params.get('cse')
             del params['cse']
-            if params.has_key('cx'):
+            typus, value = cse.split('::')
+            if params.has_key('cx') and not params.get('cx', '')==value:
                 del params['cx']
-            if params.has_key('cref'):
-                del params['cref']   
-
+            if params.has_key('cref') and not params.get('cref', '')==value:
+                del params['cref']
         
-        # if neither cx nor cref are available as parameters
+        # if neither cx nor cref are available as parameters, extract them from cse
+        # and redirect with the proper query-string
         if 'cx' not in params.keys() and 'cref' not in params.keys():
             # If there is no CSE value (page called without params),
-            # get the first defined CSE from the vocabulary
+            # get the first defined CSE from the vocabulary of CSEs
             if not cse:
                 vocab = AvailableCSEVocabularyFactory(self)
                 cse = len(vocab._terms) and vocab._terms[0].value or ''
-
+            
             # if no CSE is found in the vocabulary, show blank template
             if not cse:
                 self.cse = ''
                 return self.template()
-            
             qsparts = list()
             for key, value in params.items():
-                qsparts.append('%s=%s' %(key, value)) 
+                qsparts.append('%s=%s' %(key, value))
             qs = '&'.join(qsparts)
             typus, value = cse.split('::')
             qs = qs + '&%s=%s' %(typus,value)
             url = "%s/%s?%s" % (self.context.absolute_url(), self.template.getId(), qs)
-            print "redirect to %s" %url
             self.request.RESPONSE.redirect(url)
         
+        # we have a valid query-string; save cx and cref values, and display template
         else:
             self.cx = params.get('cx', '')
             self.cref = params.get('cref', '')
-            
             return self.template()
-
-
+    
+    
     def getCx(self):
         return self.cx
-
- 
+    
     def getCref(self):
         return self.cref
-
-
+    
+    
     def getAvailableCSE(self):
         vocab = AvailableCSEVocabularyFactory(self)
         cse = list()
@@ -83,6 +81,5 @@ class SearchResultsView(BrowserView):
             cse.append(dict(
                 title = term.title,
                 value = term.value,
-                checked = stored_value==realvalue and 'checked' or '')) 
+                checked = stored_value==realvalue and 'checked' or ''))
         return cse
-
