7ef13b6645e25ee305b1323de243a2d2b714fdaf
thomas <thomas@51ee9ba4-e84d-4896-84de-354f1821c169>
2009-06-15 14:46:08 +0000

    added possibility to add introductory title & text from a document (translatable, with fallback) to the search results page

--- slc/googlesearch/browser/search_results.py
+++ slc/googlesearch/browser/search_results.py
@@ -3,7 +3,10 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
 from slc.googlesearch.interfaces import IGoogleSearchSettings
 from slc.googlesearch.browser.settings import AvailableCSEVocabularyFactory
+from slc.googlesearch.config import RESULTS_INTRO_PAGE_NAME
 from urllib import unquote
+from Acquisition import aq_inner, aq_base
+from plone.memoize.instance import memoize
 
 
 class SearchResultsView(BrowserView):
@@ -14,6 +17,12 @@ class SearchResultsView(BrowserView):
     
     
     def __call__(self):
+        self.language = getToolByName(self.context, 'portal_languages').getPreferredLanguage()
+
+        purl = getToolByName(self.context, 'portal_url')
+        portal = purl.getPortalObject()
+        self.portal_path = '/'.join(portal.getPhysicalPath())
+        
         qs = unquote(self.request.get('QUERY_STRING'))
         parts = qs.split('&')
         params = dict()
@@ -65,7 +74,14 @@ class SearchResultsView(BrowserView):
             self.cx = params.get('cx', '')
             self.cref = params.get('cref', '')
             return self.template()
-    
+
+    @memoize
+    def _get_base(self):
+        root = self.context.restrictedTraverse(self.portal_path)
+        if hasattr(aq_base(aq_inner(root)), self.language):
+            return getattr(root, self.language).getCanonical()
+        else:
+            return root
     
     def getCx(self):
         return self.cx
@@ -95,4 +111,17 @@ class SearchResultsView(BrowserView):
             val = "cref::%s" % self.getCref()
             title = vocab.by_value.get(val) and vocab.by_value.get(val).title or ''
         return title
+    
+    
+    def getIntroDocument(self):
+        base = self._get_base()
         
+        if getattr(aq_base(base), RESULTS_INTRO_PAGE_NAME, None):
+            intro = getattr(base, RESULTS_INTRO_PAGE_NAME)
+            intro = intro.getTranslation(self.language) or intro
+            return intro
+
+        return None
+            
+
+    
--- slc/googlesearch/config.py
+++ slc/googlesearch/config.py
@@ -6,3 +6,5 @@ PROJECTNAME = 'slc.googlesearch'
 ADD_PERMISSIONS = {
     # -*- extra stuff goes here -*-
 }
+
+RESULTS_INTRO_PAGE_NAME = 'googlesearch_intro'
